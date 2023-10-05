cd7fdfefa877744642a9dd7a9f5d10026713ceba
JarekDSmall <129123790+JarekDSmall@users.noreply.github.com>
2023-10-05 15:11:09 -0400

    implemented push, pop, peek and isEmpty

--- stack.js
+++ stack.js
@@ -20,26 +20,43 @@ class Stack {
   /** push(val): add new value to end of the stack. Returns undefined. */
 
   push(val) {
-
+    const newNode = new Node(val);
+    if (!this.first) {
+      this.first = newNode;
+      this.last = newNode;
+    } else {
+      newNode.next = this.first;
+      this.first = newNode;
+    }
+    this.size++;
+    return undefined;
   }
 
   /** pop(): remove the node from the top of the stack
    * and return its value. Should throw an error if the stack is empty. */
 
   pop() {
-
+    if (!this.first) throw new Error("Stack is empty");
+    const temp = this.first;
+    if (this.first === this.last) {
+      this.last = null;
+    }
+    this.first = this.first.next;
+    this.size--;
+    return temp.val;
   }
 
   /** peek(): return the value of the first node in the stack. */
 
   peek() {
-
+    if (!this.first) return null;
+    return this.first.val;
   }
 
   /** isEmpty(): return true if the stack is empty, otherwise false */
 
   isEmpty() {
-
+    return this.size === 0;
   }
 }
 
